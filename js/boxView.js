be27bac1ab8b3603313e511918cba75dc5b68a46
Matt Thorning <matthewthorning@gmail.com>
2017-10-22 20:27:13 +0100

    added data-id  to box

--- js/boxView.js
+++ js/boxView.js
@@ -3,7 +3,10 @@ define(['marionette'], function(Marionette) {
     var BoxItem = Backbone.Marionette.ItemView.extend({
         template: _.template($('#boxListItem-tmpl').html()),
         tagName: 'li',
-        className: 'list-group-item'
+        className: 'list-group-item',
+        initialize: function() {
+            this.$el.attr('data-id', this.model.get('id'));
+        }
     });
 
     var BoxList = Backbone.Marionette.CollectionView.extend({

f61a70aaa80b0df4794ea0d46ad95fef3cf834d1
Matt Thorning <matthewthorning@gmail.com>
2017-10-27 21:36:18 +0100

    items displaying on box click

--- js/boxView.js
+++ js/boxView.js
@@ -4,14 +4,31 @@ define(['marionette', 'boxes'], function(Marionette, Boxes) {
         template: _.template($('#listItem-tmpl').html()),
         tagName: 'li',
         className: 'list-group-item',
+        events: {
+            'click': 'onBoxClick'
+        },
+        onBoxClick: function() {
+            this.model.trigger('remove:selected');
+            this.model.set({ selected: true });
+        }
     });
 
     var BoxList = Marionette.CollectionView.extend({
         tagName: 'ul',
         className: 'list-group',
         childView: Box,
+        initialize: function() {
+            this.listenTo(this.collection, 'remove:selected', this.removeSelected)
+        },  
         filter: function(child) {
             return child.get('type') === 'box';
+        },
+        removeSelected: function() {    
+            this.collection.filter(function(model) {
+                return model.get('type') === 'box' && model.get('selected');
+            }).forEach(function(model) {
+                model.set({ selected: false}, { silent: true });
+            });
         }
     });
 
--- js/itemView.js
+++ js/itemView.js
@@ -7,11 +7,21 @@ define(['marionette', 'boxes'], function(Marionette, Boxes) {
     });
 
     var ItemList = Marionette.CollectionView.extend({
+        displayBox: undefined,
         childView: Item,
         tagName: 'ul',
         className: 'list-group',
+        initialize: function() {
+            this.listenTo(this.collection, 'change', this.render)
+        },
         filter: function(model) {
-            return model.get('type') === 'item';
+            return model.get('type') === 'item' && model.get('box') === this.displayBox;
+        },
+        onBeforeRender: function() {
+            var selectedBox = this.collection.findWhere({ selected: true });
+            if(selectedBox) {
+                this.displayBox = selectedBox.get('name');
+            }
         }
     });
 
